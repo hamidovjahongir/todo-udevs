b953f060fdf6ead087b3f1bede561817123e47a8
khamidov <uzbff460@gmail.com>
2025-09-06 22:09:42 +0500

    bags fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:todo/core/di/injection_container.dart';
-import 'package:todo/features/app/application.dart';
+import 'package:todo/app/application.dart';
 import 'package:todo/features/home/data/datasources/local_datasource.dart';
 
 void main() async {
